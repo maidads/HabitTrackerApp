7522edd0d5665b6042d574f458ec203cbba05751
Maida <98819095+maidads@users.noreply.github.com>
2024-05-12 22:15:02 +0200

    update color

--- HabitTracker/CalendarView.swift
+++ HabitTracker/CalendarView.swift
@@ -30,63 +30,86 @@ struct CalendarView: View {
 }
 
 
-import SwiftUI
-
 struct HabitCalendarView: View {
     @ObservedObject var habit: Item
     @Environment(\.calendar) var calendar
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var trackedDatesSet: Set<String> = []
 
-    private var dayFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "d"
-        return formatter
-    }
+    init(habit: Item) {
+            _habit = ObservedObject(initialValue: habit)
+        }
+    static private var dayFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "d"
+            return formatter
+        }()
 
-    private var headerFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "EEE"
-        return formatter
-    }
+        static private var headerFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "EEE"
+            return formatter
+        }()
 
     var body: some View {
         VStack {
             Text("Calendar for \(habit.name ?? "Habit")").font(.headline)
-            HStack(spacing: 0) {
-                ForEach(daysOfTheWeek(), id: \.self) { day in
-                    Text(headerFormatter.string(from: day))
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .padding(.vertical, 8)
-                        .font(.caption)
-                }
-            }
-            LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7), spacing: 10) {
-                ForEach(daysInMonth(Date()), id: \.self) { day in
-                    Text(dayFormatter.string(from: day))
-                        .frame(width: 30, height: 30)
-                        .padding(8)
-                        .background(Circle()
-                                        .fill(isToday(day) ? Color.blue : (isHabitTrackedOn(day) ? Color.green : Color.gray.opacity(0.2))))
-                        .foregroundColor(Color.white)
-                }
+            headersView
+            calendarGrid
+        }
+        .onAppear {
+            updateTrackedDatesSet()
+        }
+    }
+
+    private var headersView: some View {
+        HStack(spacing: 0) {
+            ForEach(daysOfTheWeek(), id: \.self) { day in
+                Text(Self.headerFormatter.string(from: day))
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding(.vertical, 8)
+                    .font(.caption)
             }
         }
+    }
+
+    private var calendarGrid: some View {
+        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7), spacing: 10) {
+            ForEach(daysInMonth(Date()), id: \.self) { day in
+                dayCell(for: day)
+            }
+        }
+    }
+
+    private func dayCell(for day: Date) -> some View {
+        let dayIsSelected = trackedDatesSet.contains(dateString(day))
+        return Text(Self.dayFormatter.string(from: day))
+            .frame(width: 30, height: 30)
+            .padding(8)
+            .background(Circle()
+                .fill(dayIsSelected ? Color.green : Color.gray.opacity(0.2)))
+            .foregroundColor(Color.white)
+            .onTapGesture {
+                toggleDaySelected(day: day)
+            }
+    }
+
+
+    private func updateTrackedDatesSet() {
+        trackedDatesSet = Set(habit.trackedDates?.split(separator: ",").map(String.init) ?? [])
+    }
+
+    private func dateString(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: date)
     }
 
     private func daysInMonth(_ date: Date) -> [Date] {
         guard let monthInterval = calendar.dateInterval(of: .month, for: date) else {
             return []
         }
-        return calendar.generateDates(
-            inside: monthInterval,
-            matching: DateComponents(hour: 0, minute: 0, second: 0)
-        )
-    }
-
-    private func isHabitTrackedOn(_ date: Date) -> Bool {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        let dateString = dateFormatter.string(from: date)
-        return habit.trackedDates?.contains(dateString) ?? false
+        return calendar.generateDates(inside: monthInterval, matching: DateComponents(hour: 0, minute: 0, second: 0))
     }
 
     private func isToday(_ date: Date) -> Bool {
@@ -96,9 +119,30 @@ struct HabitCalendarView: View {
     private func daysOfTheWeek() -> [Date] {
         let today = Date()
         let currentWeekday = calendar.component(.weekday, from: today)
-        let days = (1...7).map { calendar.date(byAdding: .day, value: $0 - currentWeekday, to: today)! }
-        return days
+        return (1...7).map { calendar.date(byAdding: .day, value: $0 - currentWeekday, to: today)! }
     }
+
+    func toggleDaySelected(day: Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let dateString = dateFormatter.string(from: day)
+
+        var newTrackedDates = Set(habit.trackedDates?.split(separator: ",").map(String.init) ?? [])
+        if newTrackedDates.contains(dateString) {
+            newTrackedDates.remove(dateString)
+        } else {
+            newTrackedDates.insert(dateString)
+        }
+
+        habit.trackedDates = newTrackedDates.joined(separator: ",")
+        do {
+            try viewContext.save()
+        } catch {
+            print("Failed to update habit tracked dates: \(error)")
+        }
+        trackedDatesSet = newTrackedDates
+    }
+
 }
 
 
@@ -119,4 +163,5 @@ extension Calendar {
         return dates
     }
 }
+
 
