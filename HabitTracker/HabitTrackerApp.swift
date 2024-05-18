e23761a2289edbded3cc9f2234182e9b59bc62d1
Maida <98819095+maidads@users.noreply.github.com>
2024-05-18 02:01:55 +0200

    asking for permission in the beginning for notifications

--- HabitTracker/HabitTrackerApp.swift
+++ HabitTracker/HabitTrackerApp.swift
@@ -5,7 +5,9 @@
 //  Created by Maida on 2024-05-04.
 //
 
+import UIKit
 import SwiftUI
+import UserNotifications
 
 @main
 struct HabitTrackerApp: App {
@@ -16,5 +18,16 @@ struct HabitTrackerApp: App {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print("Request Authorization Failed (\(error), \(error.localizedDescription))")
+            }
+        }
+        return true
     }
 }
