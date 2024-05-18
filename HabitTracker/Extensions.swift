c442cbbe6fea1ffd69cc056b7e5ff76893bfa25b
Maida <98819095+maidads@users.noreply.github.com>
2024-05-18 02:13:33 +0200

    moved extensions to own file for color

---
+++ HabitTracker/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  HabitTracker
+//
+//  Created by Maida on 2024-05-18.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        (r, g, b) = (int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        self.init(red: Double(r) / 255, green: Double(g) / 255, blue: Double(b) / 255)
+    }
+}
+
+extension UIColor {
+    func toHex() -> String? {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        return String(format: "#%02X%02X%02X", Int(red * 255), Int(green * 255), Int(blue * 255))
+    }
+    convenience init(color: Color) {
+            let uiColor = UIColor(color)
+            self.init(cgColor: uiColor.cgColor)
+        }
+}
